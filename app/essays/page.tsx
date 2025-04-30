c959e70bb60ef8b438910cdb16a6d6718fc06adc
ALI-HUP <ali.sheikhbahaie84@gmail.com>
2025-04-30 22:10:13 +0330

    eassay page

--- app/essays/page.tsx
+++ app/essays/page.tsx
@@ -1,24 +1,31 @@
 import React from "react";
+import Header from "@/components/Header";
+
 
 export default function EssaysPage() {
   return (
-    <div className="min-h-screen p-6 bg-white text-gray-800">
-      <h1 className="text-3xl font-bold mb-6 text-center">Reading & Plans</h1>
+    <div className="min-h-screen p-5 flex flex-col items-center">
+      <Header />
 
-      <div className="grid gap-4 md:grid-cols-2">
-        <div className="p-4 border rounded shadow">
-          <h2 className="text-xl font-semibold mb-2">Workout Plan - Week 1</h2>
-          <p className="text-sm">A simple guide to start your strength training journey.</p>
-        </div>
 
-        <div className="p-4 border rounded shadow">
-          <h2 className="text-xl font-semibold mb-2">Nutrition Guide</h2>
-          <p className="text-sm">Basics of clean eating for athletes and active people.</p>
-        </div>
+      <div className="mt-10">
+        <h1 className="text-3xl font-bold mb-10 text-center">Reading & Plans</h1>
 
-        <div className="p-4 border rounded shadow">
-          <h2 className="text-xl font-semibold mb-2">Mental Strength Essay</h2>
-          <p className="text-sm">How mindset impacts performance and how to train it.</p>
+        <div className="grid gap-4 md:grid-cols-2">
+          <div className="p-5 rounded-lg shadow bg-white text-black w-[600px]">
+            <h2 className="text-xl font-semibold mb-4">Road to Pro - Week 1</h2>
+            <p className="text-md">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur, cumque quae voluptate sint molestiae quas voluptatibus dignissimos illo sequi dolor rem modi non quod tempore optio. Voluptatibus eaque ex provident!</p>
+          </div>
+
+          <div className="p-5 rounded-lg shadow bg-white text-black w-[600px]">
+            <h2 className="text-xl font-semibold mb-4">Road to Pro - week 2</h2>
+            <p className="text-md">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur, cumque quae voluptate sint molestiae quas voluptatibus dignissimos illo sequi dolor rem modi non quod tempore optio. Voluptatibus eaque ex provident!</p>
+          </div>
+
+          <div className="p-5 rounded-lg shadow bg-white text-black w-[600px]">
+            <h2 className="text-xl font-semibold mb-4">Road to Pro - week 3</h2>
+            <p className="text-md">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur, cumque quae voluptate sint molestiae quas voluptatibus dignissimos illo sequi dolor rem modi non quod tempore optio. Voluptatibus eaque ex provident!</p>
+          </div>
         </div>
       </div>
     </div>
