9d246b628e85de0f0364efc0def813c8a123883b
ALI-HUP <ali.sheikhbahaie84@gmail.com>
2025-04-30 22:21:05 +0330

    backgound update

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="w-[60%] m-5 p-5 flex justify-between items-center bg-red-950/30 rounded-full">
+    <header className="w-[60%] m-5 p-5 flex justify-between items-center bg-red-700/50 rounded-full">
       <Link href="/" className="text-3xl font-bold">Misfit</Link>
       <nav className="text-md font-semibold space-x-6">
         <Link href="/essays" className="hover:underline">Essays</Link>
