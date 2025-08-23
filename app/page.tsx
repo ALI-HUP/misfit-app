dd83a1cc0c1329f0fe3d6603a5e484c1ee6a2988
ALI-HUP <ali.sheikhbahaie84@gmail.com>
2025-08-23 21:32:10 +0330

    edit

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
           className="px-4 py-3 sm:px-5 sm:py-4 my-6 sm:my-8 md:my-10 
                      bg-yellow-300 text-black rounded-lg font-bold 
                      hover:bg-yellow-200 transition-colors
-                     transform transition-transform duration-300 hover:scale-[1.03] active:scale-[0.98]
+                     transform duration-300 hover:scale-[1.03] active:scale-[0.98]
                      text-sm sm:text-base md:text-lg"
         >
           Get Started...
