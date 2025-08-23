7fc9631c69885f6404c3e4523959777b0882e495
ALI-HUP <ali.sheikhbahaie84@gmail.com>
2025-08-23 16:20:54 +0330

    header and color correction

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,9 +4,9 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="w-[60%] m-5 p-5 flex justify-between items-center bg-red-700/50 rounded-full">
-      <Link href="/" className="text-3xl font-bold">Misfit</Link>
-      <nav className="text-md font-semibold space-x-6">
+    <header className="w-[60%] m-6 p-4 px-7 flex justify-between items-center bg-red-600/70 rounded-full">
+      <Link href="/" className="text-4xl font-bold">Misfit</Link>
+      <nav className="text-lg space-x-8">
         <Link href="/essays" className="hover:underline">Essays</Link>
         <Link href="/videos" className="hover:underline">Videos</Link>
         <Link href="/contact" className="hover:underline">Contact</Link>
