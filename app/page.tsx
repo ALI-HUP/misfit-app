b1f2e3a8541443f82c2618fd2cee8bfa971d0c32
ALI-HUP <ali.sheikhbahaie84@gmail.com>
2025-08-24 16:44:47 +0330

    home page responsive improvment

--- app/page.tsx
+++ app/page.tsx
@@ -10,12 +10,12 @@ export default function Home() {
     <div className="flex flex-col items-center">
       <Header />
 
-      <main className="text-center flex flex-col items-center px-4">
+      <main className="text-center flex flex-col items-center px-7">
         <h2 className="text-3xl sm:text-4xl md:text-5xl font-extrabold my-6 sm:my-8 md:my-10">
           Welcome to Misfit!
         </h2>
 
-        <div className="text-base sm:text-lg md:text-xl max-w-xl">
+        <div className="text-base sm:text-lg md:text-xl">
           <p>One Step Away from Your Goal!!!</p>
           <p>Get stronger, faster, and more focused. Let’s push limits together. 💪🔥</p>
         </div>
@@ -25,43 +25,31 @@ export default function Home() {
           className="px-4 py-3 sm:px-5 sm:py-4 my-6 sm:my-8 md:my-10 
           bg-yellow-300 text-black rounded-lg font-bold 
           hover:bg-yellow-200 transition-colors
-            transform duration-300 hover:scale-[1.03] active:scale-[0.98]
-            text-sm sm:text-base md:text-lg"
-          >
+          transform duration-300 hover:scale-[1.03] active:scale-[0.98]
+          text-sm sm:text-base md:text-lg"
+        >
           Get Started...
         </Link>
       </main>
 
-      <section className="flex flex-wrap justify-center gap-8 sm:gap-12 md:gap-20 text-center font-bold w-full px-4">
-        <div className="flex flex-col items-center">
-          <Link href="https://www.youtube.com/@parsabolor/videos">
-            <div 
-              className="flex flex-col gap-2 transform transition-transform duration-300 hover:scale-[1.05] active:scale-[0.98]"
-            >
-              <Image 
-                src={MotIcon} 
-                alt="Sponsor" 
-                className="w-20 h-20 sm:w-28 sm:h-28 md:w-32 md:h-32 rounded-full border-4 border-white shadow-md" 
-              />
-              <label className="text-sm sm:text-base md:text-lg">Be a Gymbo</label>
-            </div>
-          </Link>
-        </div>
+      <section className="my-5 grid grid-cols-1 sm:grid-cols-2 gap-10 sm:gap-16 md:gap-20 md:my-0 justify-items-center w-[50%] px-4 text-center font-bold">
+        <Link href="https://www.youtube.com/@parsabolor/videos" className="flex flex-col items-center transform transition-transform duration-300 hover:scale-[1.05] active:scale-[0.98]">
+          <Image 
+            src={MotIcon} 
+            alt="Sponsor" 
+            className="w-20 h-20 xs:w-24 xs:h-24 sm:w-28 sm:h-28 md:w-32 md:h-32 rounded-full border-4 border-white shadow-md object-cover" 
+          />
+          <label className="mt-2 text-xs xs:text-sm sm:text-base md:text-lg">Be a Gymbo</label>
+        </Link>
 
-        <div className="flex flex-col items-center">
-          <Link href="https://yalshop.com/">
-            <div 
-              className="flex flex-col gap-2 transform transition-transform duration-300 hover:scale-[1.05] active:scale-[0.98]"
-            >
-              <Image 
-                src={YalIcon} 
-                alt="Sponsor" 
-                className="w-20 h-20 sm:w-28 sm:h-28 md:w-32 md:h-32 rounded-full border-4 border-white shadow-md" 
-              />
-              <label className="text-sm sm:text-base md:text-lg">Our Sponsor: YAL</label>
-            </div>
-          </Link>
-        </div>
+        <Link href="https://yalshop.com/" className="flex flex-col items-center transform transition-transform duration-300 hover:scale-[1.05] active:scale-[0.98]">
+          <Image 
+            src={YalIcon} 
+            alt="Sponsor" 
+            className="w-20 h-20 xs:w-24 xs:h-24 sm:w-28 sm:h-28 md:w-32 md:h-32 rounded-full border-4 border-white shadow-md object-cover" 
+          />
+          <label className="mt-2 text-xs xs:text-sm sm:text-base md:text-lg">Our Sponsor: YAL</label>
+        </Link>
       </section>
     </div>
   );
